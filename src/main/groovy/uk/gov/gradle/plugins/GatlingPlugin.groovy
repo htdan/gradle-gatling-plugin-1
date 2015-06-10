f0113b7c0923169740fadd32f42fb0445f7107ee
Haitao Dan <haitao.dan@metaback.com>
2015-06-10 14:45:12 +0100

    Add include and exclude filters

--- src/main/groovy/uk/gov/gradle/plugins/GatlingPlugin.groovy
+++ src/main/groovy/uk/gov/gradle/plugins/GatlingPlugin.groovy
@@ -3,96 +3,157 @@ package uk.gov.gradle.plugins
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import java.util.regex.Pattern
+
 class GatlingPlugin implements Plugin<Project> {
-	final String GATLING_VERSION = '2.1.3'
 
-	private String gatlingReportsDirectory
-	private Project project
+    final String GATLING_VERSION = '2.1.3'
 
-	void apply(Project project) {
-		this.project = project
-		project.plugins.apply 'scala'
-		project.extensions.create('gatling', GatlingPluginExtension)
-		project.dependencies {
-			testCompile "io.gatling.highcharts:gatling-charts-highcharts:${GATLING_VERSION}",
-					'com.nimbusds:nimbus-jose-jwt:2.22.1'
-		}
-		project.repositories {
+    private String gatlingReportsDirectory
+    private Project project
+
+    void apply(Project project) {
+        this.project = project
+        project.plugins.apply 'scala'
+        project.extensions.create('gatling', GatlingPluginExtension)
+        project.dependencies {
+            testCompile "io.gatling.highcharts:gatling-charts-highcharts:${GATLING_VERSION}",
+                        'com.nimbusds:nimbus-jose-jwt:2.22.1'
+        }
+        project.repositories {
             jcenter()
             mavenLocal()
             mavenCentral()
         }
         project.sourceSets {
             test {
-				scala {
-					srcDirs = ['user-files/simulations']
-				}
-				resources{
-					srcDirs = ['user-files/conf', 'user-files/data']
-				}
+                scala {
+                    srcDirs = ['user-files/simulations']
+                }
+                resources {
+                    srcDirs = ['user-files/conf', 'user-files/data']
+                }
                 // change the build direcotry to gatling default
                 output.classesDir = 'target/test-classes'
             }
         }
         gatlingReportsDirectory = "$project.buildDir.absolutePath/gatling-reports"
-		project.task('gatlingTest',
-				dependsOn:'build') << {
-			project.gatling.verifySettings()
-			final def sourceSet = project.sourceSets.test
-			final def gatlingRequestBodiesDirectory = firstPath(sourceSet.resources.srcDirs) + "/bodies"
-			final def gatlingClasspath = sourceSet.output + sourceSet.runtimeClasspath
-			final def scenarios = project.gatling._scenarios ?: getGatlingScenarios(sourceSet)
+        project.task('gatlingTest',
+                     dependsOn: 'build') << {
+            project.gatling.verifySettings()
+            final def sourceSet = project.sourceSets.test
+            final def gatlingRequestBodiesDirectory = firstPath(sourceSet.resources.srcDirs) + "/bodies"
+            final def gatlingClasspath = sourceSet.output + sourceSet.runtimeClasspath
+            final def scenarios = project.gatling._scenarios ?: getGatlingScenarios(sourceSet)
 
-			logger.lifecycle "Executing gatling scenarios: $scenarios"
-			scenarios?.each { scenario ->
-				project.javaexec {
-					main = 'io.gatling.app.Gatling'
-					classpath = gatlingClasspath
-					if(project.gatling.verbose) jvmArgs '-verbose'
-					// If a user has the GATLING_HOME env var set, gradle will try to compile
-					// simulations which are saved in GATLING_HOME.  This can break the build.
-					environment GATLING_HOME:''
-					args '-rf', gatlingReportsDirectory,
-							'-s', scenario,
-							'-bdf', gatlingRequestBodiesDirectory
-					systemProperties(project.gatling.systemProperties ?: [:])
-				}
-			}
-			logger.lifecycle "Gatling scenarios completed."
-		}
-		project.task('openGatlingReport') << {
-			def mostRecent
-			withGatlingReportsDirs { projectDir ->
-				if(projectDir > mostRecent) {
-					mostRecent = projectDir
-				}
-			}
-			openReport mostRecent
-		}
-		project.task('openGatlingReports') << {
-			withGatlingReportsDirs openReport
-		}
-	}
+//            project.hasProperty("includex")? logger.info(project.getProperties().get('includex')): logger.info("include is not here")
+            def include = project.getProperties().get('include')
+            def exclude = project.getProperties().get('exclude')
+            def whiteListPattern = createPatternFromList(include==null?null:Arrays.asList(include));
+            def blackListPattern = createPatternFromList(exclude==null?null:Arrays.asList(exclude));
 
-	private getGatlingScenarios(sourceSet) {
-		final String scenarioSrcDir = "$project.projectDir.absolutePath/user-files/simulations"
-		final int scenarioPathPrefix = "$scenarioSrcDir/".size()
-		final int scenarioPathSuffix = - ('.scala'.size() + 1)
-		sourceSet.allScala.files*.toString().
-				findAll { it.endsWith 'Scenario.scala' }.
-				collect { it[scenarioPathPrefix..scenarioPathSuffix] }*.
-				replace('/', '.')
-	}
+            logger.info 'Include ' + whiteListPattern
+            logger.info 'Exclude ' + blackListPattern
 
-	private firstPath(Set<File> files) {
-		return files.toList().first().toString()
-	}
+            logger.lifecycle "Scenarios found before filter: $scenarios"
+            scenarios?.each { scenario ->
+                if (check(scenario, blackListPattern, whiteListPattern)) {
+                    logger.lifecycle "Scenario to be executed: $scenario"
+                    project.javaexec {
+                        main = 'io.gatling.app.Gatling'
+                        classpath = gatlingClasspath
+                        if (project.gatling.verbose) {
+                            jvmArgs '-verbose'
+                        }
+                        // If a user has the GATLING_HOME env var set, gradle will try to compile
+                        // simulations which are saved in GATLING_HOME.  This can break the build.
+                        environment GATLING_HOME: ''
+                        args '-rf', gatlingReportsDirectory,
+                             '-s', scenario,
+                             '-bdf', gatlingRequestBodiesDirectory
+                        systemProperties(project.gatling.systemProperties ?: [:])
+                    }
+                }
+            }
+            logger.lifecycle "Gatling scenarios completed."
+        }
+        project.task('openGatlingReport') << {
+            def mostRecent
+            withGatlingReportsDirs { projectDir ->
+                if (projectDir > mostRecent) {
+                    mostRecent = projectDir
+                }
+            }
+            openReport mostRecent
+        }
+        project.task('openGatlingReports') << {
+            withGatlingReportsDirs openReport
+        }
+    }
 
-	private openReport = { reportDir ->
-		project.exec { commandLine 'open', "$reportDir/index.html" }
-	}
+    private getGatlingScenarios(sourceSet) {
+        final String scenarioSrcDir = "$project.projectDir.absolutePath/user-files/simulations"
+        final int scenarioPathPrefix = "$scenarioSrcDir/".size()
+        final int scenarioPathSuffix = -('.scala'.size() + 1)
+        sourceSet.allScala.files*.toString().
+                findAll { it.endsWith 'Scenario.scala' }.
+                collect { it[scenarioPathPrefix..scenarioPathSuffix] }*.
+                replace('/', '.')
+    }
 
-	private withGatlingReportsDirs(Closure c) {
-		new File(gatlingReportsDirectory).eachDirMatch(~/.*-\d+/, c)
-	}
+    private firstPath(Set<File> files) {
+        return files.toList().first().toString()
+    }
+
+    private openReport = { reportDir ->
+        project.exec { commandLine 'open', "$reportDir/index.html" }
+    }
+
+    private withGatlingReportsDirs(Closure c) {
+        new File(gatlingReportsDirectory).eachDirMatch(~/.*-\d+/, c)
+    }
+
+    static boolean check(String clazz, Pattern blackListPattern, Pattern whiteListPattern) {
+        if (inWhiteList(clazz, whiteListPattern)) {
+            println "---- " + clazz + "  in white list. ----"
+            return true;
+        } else {
+            if (inBlackList(clazz, blackListPattern)) {
+                println "---- " + clazz + "  in black list. ----"
+                return false;
+            } else {
+                println "---- " + clazz + "  not in black and white list. ----"
+                return true;
+            }
+        }
+    }
+
+    static boolean inBlackList(String path, Pattern blackListPattern) {
+        if (blackListPattern == null) {
+            return false
+        };
+        return blackListPattern.matcher(path).matches();
+    }
+
+    static boolean inWhiteList(String path, Pattern whiteListPattern) {
+        if (whiteListPattern == null) {
+            return false
+        };
+        return whiteListPattern.matcher(path).matches();
+    }
+
+    static Pattern createPatternFromList(List<String> list) {
+        if (list == null || list.size() == 0) {
+            return null
+        };
+        StringBuilder sb = new StringBuilder(500);
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+            if (i < list.size() - 1) {
+                sb.append("|")
+            };
+        }
+        return Pattern.compile(sb.toString());
+    }
+
 }
